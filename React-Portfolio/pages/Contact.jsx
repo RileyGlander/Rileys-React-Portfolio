c74f41470cd4b56b087e8482a3b9829aca82e4d1
RileyGlander <142702948+RileyGlander@users.noreply.github.com>
2024-02-05 15:42:07 -0600

    changed a tags to link tags and the hrefs to the correct links

--- React-Portfolio/pages/Contact.jsx
+++ React-Portfolio/pages/Contact.jsx
@@ -1,21 +1,74 @@
-export default function Contact() {
-    return (
-      <div>
-        <h1>Contact Page</h1>
-        <p>
-          Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-          molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-          magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-          efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-          mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-          posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-          faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-          ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-          dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-          conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-          rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-        </p>
-      </div>
-    );
-  }
+import { useState } from "react"
+
+function Form() {
+    const [email, setEmail] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
+    
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+    
+
+if (!email || !firstName) {
+    setErrorMessage("Missing Information")
+    return
+}
+setErrorMessage("")
+alert(email + " " + firstName)
+setEmail("")
+setFirstName("")
+}
+const handleInputChange = (e) => {
+    const inputName = e.target.name;
+    const inputValue = e.target.value;
+    if(inputName==="email"){
+        setEmail(inputValue)
+    }
+    else if (inputName==="firstName"){
+        setFirstName(inputValue)
+    }
+
+    setEmail(inputValue)
+    console.log(inputName, inputValue)
+}
+
+return (
+    <div>
+        <form onSubmit={handleFormSubmit}>
+            <input type="text" name="email" onCHange={handleInputChange}/><br/>
+            <input type="text" name="firstName" onCHange={handleInputChange}/><br/>
+            <button type ="submit"> ADD EMAIL</button>
+            {errorMessage && (
+                <div style={{color:"red"}}>ERROR: {errorMessage}</div>
+            )}
+        </form>
+    </div>
+    )
+}
+
+export default Form;
+
+
+
+
+// export default function Contact() {
+//     return (
+//       <div>
+//         <h1>Contact Page</h1>
+//         <p>
+//           Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
+//           molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
+//           magna a ultrices. Aenean pellentesque placerat lacus imperdiet
+//           efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
+//           mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
+//           posuere, eget tristique dui dapibus. Maecenas fermentum elementum
+//           faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
+//           ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
+//           dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
+//           conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
+//           rhoncus. Etiam vel condimentum magna, quis tempor nulla.
+//         </p>
+//       </div>
+//     );
+//   }
   
--- React-Portfolio/src/main.jsx
+++ React-Portfolio/src/main.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import './index.css'
+import AboutMe from '../pages/AboutMe';
+import Contact from '../pages/Contact';
+import Portfolio from '../pages/Portfolio';
+import Resume from '../pages/Resume';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />,
+      },
+      {
+        path: '/contact',
+        element: <Contact/>,
+      },
+      {
+        path: '/portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
