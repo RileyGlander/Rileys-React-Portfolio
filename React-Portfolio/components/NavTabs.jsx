16a7bcdfc5eae8fe8a971509b794405e09199dae
RileyGlander <142702948+RileyGlander@users.noreply.github.com>
2024-02-06 20:54:01 -0600

    added netlify file and adding finishing touches

--- React-Portfolio/components/NavTabs.jsx
+++ React-Portfolio/components/NavTabs.jsx
@@ -10,7 +10,7 @@ function Navigation({ currentPage, handlePageChange }) {
           // This is a conditional (ternary) operator that checks to see if the current page is "Home"
           // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
           className={currentPage === '/' ? 'nav-link active' : 'nav-link'}> 
-          Home
+          About Me
         </Link>
       </li>
         <li className="nav-item">
